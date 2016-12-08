9769b6da8d472c7fcfcd2407bf58b33468b8b509
GuilhermeSilvaCosta <tec.guilherme.dsc@gmail.com>
2016-12-07 23:53:19 -0200

    Capitulo 4

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var routes = require('./routes');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes.index);
+app.use('/usuarios', routes.user.index);
+
+app.listen(3000, function(){
+  console.log("Ntalk no ar.");
+});
