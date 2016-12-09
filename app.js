506a5c1c81be5f417865e26e80f7f557b2268a01
GuilhermeSilvaCosta <tec.guilherme.dsc@gmail.com>
2016-12-08 23:53:39 -0200

    capitulo 5.2 completo

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
-var express = require('express');
-var routes = require('./routes');
-var app = express();
+const express = require('express');
+// const routes = require('./routes');
+
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+
+const load = require('express-load');
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+app.use(cookieParser('ntalk'));
+app.use(session());
+// app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.urlencoded());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 
-app.use('/', routes.index);
-app.use('/usuarios', routes.user.index);
+// app.use('/', routes.index);
+// app.use('/usuarios', routes.user.index);
+
+load('models')
+  .then('controllers')
+  .then('routes')
+  .into(app);
 
 app.listen(3000, function(){
   console.log("Ntalk no ar.");
