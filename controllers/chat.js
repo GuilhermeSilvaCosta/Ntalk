0055453c2c165b95269c8d4acc99eee28b1c9492
GuilhermeSilvaCosta <tec.guilherme.dsc@gmail.com>
2016-12-21 00:41:34 -0200

    Capitulo 6.7 OK

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,9 +1,10 @@
 module.exports = function(app){
     const ChatController = {
         index: function(req, res){
-            const params = {
-                email: req.params.email
-            };
+            // const params = {
+            //     email: req.params.email
+            // };
+            var params = {sala: req.query.sala};            
             res.render('chat/index', params);
         }
     };
--- sockets/chat.js
+++ sockets/chat.js
@@ -1,23 +1,31 @@
+'use strict';
+
 module.exports = function(io) {
-    const sockets = io.sockets,
-          crypto = require('crypto'),
-          md5 = crypto.createHash('md5');
+    const sockets = io.sockets;
+    let crypto = require('crypto')          
     sockets.on('connection', function (client) {    
         const session = client.request.session,
               usuario = session.usuario;
+        client.email = usuario.email;
+
+        const onlines = Object.keys(sockets.connected);        
+        onlines.forEach(function(online){
+            let on = sockets.sockets[online];            
+            client.emit('notify-onlines', on.email);
+            client.broadcast.emit('notify-onlines', on.email);
+        });
+
         client.on('join', function(sala){
-            if(sala){
-                sala = sala.replace('?', '');
-            }else{
-                const timestamp = new Date().toString();
+            if(!sala){
+                let timestamp = new Date().toString(),
+                    md5 = crypto.createHash('md5');;
                 sala = md5.update(timestamp).digest('hex');
-            }
-            session.sala = sala; 
-            client.set('sala', sala);
+            } 
+            client.sala = sala;
             client.join(sala);
         });
-        client.on('send-server', function (msg) {
-            const sala = session.sala;
+        client.on('send-server', function (msg) {            
+            const sala = client.sala;
             msg = "<b>"+usuario.nome+":</b> "+msg+"<br>";
             if(sala){
                 const data = {email: usuario.email,
@@ -27,12 +35,13 @@ module.exports = function(io) {
             }
         });
         client.on('disconnect', function(){
-            const sala = session.sala;
-            // client.get('sala', function(erro, sala){
+            client.broadcast.emit('notify-offline', usuario.email);
+            const sala = client.sala;            
             if (sala){
+                const msg = "<b>" + usuario.nome + "</b> saiu.<br>";                
+                sockets.in(sala).emit('send-client', msg);
                 client.leave(sala);
-            }
-            // });
+            }            
         });
     });
 }
